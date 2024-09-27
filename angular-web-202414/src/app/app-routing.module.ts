e94d70d51b5a834c0f078160a4076113ae7218fe
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2024-09-26 19:02:26 -0500

    chore: add routing of login and recover password views

--- angular-web-202414/src/app/app-routing.module.ts
+++ angular-web-202414/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'recover-password', component: RecoverPasswordComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
